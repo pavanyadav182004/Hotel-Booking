d4ae1ed6024fa288febacd8db33c194bbb055ec0
PavanYadav <pavanyadav182004@gmail.com>
2026-03-06 11:54:22 +0530

    g

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import assets, { cities } from '../assets/assets';
 
 const Hero = () => {
+
+
+    // Form control
+
+
+
+
+
     return (
+
+        // Main div-1
         <div className='flex flex-col items-start justify-center px-6 
     md:px-16 lg:px-24 xl:px-32 text-white 
     bg-[url("/src/assets/hotel.jpg")] 
@@ -10,6 +20,7 @@ const Hero = () => {
 
             {/* Dark Overlay */}
 
+            {/* second-div */}
             <div>
                 <p className='bg-[#49b9ff]/50 px-3.5 py-1 w-70 rounded-full mt-20'>
                     The  Ultimate Hotel Experience</p>
@@ -21,15 +32,25 @@ const Hero = () => {
 
 
                 {/* Form */}
+
+
                 <form className='bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 flex flex-col 
                 md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
 
+
+                    {/* Form First-div */}
                     <div>
+
+                        {/*  First-div-1 */}
                         <div className='flex items-center gap-2'>
                             <img src={assets.calenderIcon} alt="" className='h-4' />
                             <label htmlFor="destinationInput">Destination</label>
                         </div>
-                        <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
+
+                        {/* Input-1 */}
+                        <input
+
+                            list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
                         <datalist id='destinations'>
                             {cities.map((city, index) => (
                                 <option value={city} key={index} />
@@ -38,32 +59,52 @@ const Hero = () => {
                         </datalist>
                     </div>
 
+
+
                     <div>
                         <div className='flex items-center gap-2'>
                             <img src={assets.calenderIcon} alt="" className='h-4' />
                             <label htmlFor="checkIn">Check in</label>
                         </div>
-                        <input id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+
+
+                        <input
+
+                            id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
                     </div>
+
+
 
                     <div>
                         <div className='flex items-center gap-2'>
                             <img src={assets.calenderIcon} alt="" className='h-4' />
                             <label htmlFor="checkOut">Check out</label>
                         </div>
-                        <input id="checkOut" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+
+                        <input
+
+                            id="checkOut" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
                     </div>
+
+
 
                     <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
                         <label htmlFor="guests">Guests</label>
-                        <input min={1} max={4} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
+                        <input
+
+
+                            min={1} max={4} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
                     </div>
+
+
 
                     <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
                         <img src={assets.searchIcon} alt="search-icon" className='h-7' />
                         <span>Search</span>
                     </button>
                 </form>
+
+
 
             </div>
 
